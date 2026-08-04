96ce8913f5d45f80d413a00b62fdf0d1222820f7
6710510461 <wut.nanny2260@gmail.com>
2026-08-04 11:29:26 +0700

    Add LIFF_ID and WEB_APP_URL constants

--- js/config.js
+++ js/config.js
@@ -1,1 +1,5 @@
+    const LIFF_ID =
+      "2010089876-qsjILd9E";
 
+    const WEB_APP_URL =
+      "https://script.google.com/macros/s/AKfycbydVoWAsCrdAb4n6hKQErxU869TeUgMyuwqrTrBRycXdU0_oqlN0M8D_fSvFurTd9C1WQ/exec";
